148d1269e3117b08251bdf61bda06ef7e3eacd7b
Keltoummalouki <keltoummalouki911@gmail.com>
2025-01-13 14:02:20 +0100

    login

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -4,11 +4,33 @@ namespace App\Controllers;
 
 use App\Classes\User;
 use App\Config\DatabaseConnexion;
+use App\Models\UserModel;
 use App\Models\NewUserModel;
 use PDO;
 
 class AuthController {
     
+    public function login($email, $password) {
+        $userModel = new UserModel();
+        $user = $userModel->findUser($email, $password);
+        
+        if ($user !== null) {
+            switch ($user->getRole()) {
+                case "Admin":
+                    header('Location: ../admin/dashboard.php');
+                    exit();
+                case "Teacher":
+                    header("Location: ../teacher/index.php");
+                    exit();
+                case "Student":
+                    header("Location: ../student/index.php");
+                    exit();
+            }
+        } else {
+            header('Location: ../auth/login.php?error=invalid_credentials');
+            exit();
+        }
+    }
 
     public function register($username, $email, $password, $role) {
         $newUser = new NewUserModel();
